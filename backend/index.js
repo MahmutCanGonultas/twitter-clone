a4120f97644e1e41025328e185a797458921d67f
Blackfyre33 <mahmutcangonultas@outlook.com>
2026-05-23 23:18:55 +0300

    feat: add auth routes with bcrypt and jwt middleware

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+const authRoutes = require('./routes/auth');
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Server is runnig' });
---
+++ backend/routes/auth.js
@@ -0,0 +1,58 @@
+const jwt = require('jsonwebtoken');
+const authMiddleware = require('../middleware/auth');
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const pool = require('../db');
+
+router.post('/register', async (req, res) => {
+  try {
+    const { email, username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const result = await pool.query(
+      'INSERT INTO users (email, username, password) VALUES ($1, $2, $3) RETURNING id, email, username',
+      [email, username, hashedPassword],
+    );
+    res.status(201).json({ user: result.rows[0] });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const result = await pool.query('SELECT * FROM users WHERE email =$1', [
+      email,
+    ]);
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    const user = result.rows[0];
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' },
+    );
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.get('/me', authMiddleware, async (req, res) => {
+  res.json({ user: req.user });
+});
+
+module.exports = router;
