d8937dc2ccc231ac2b1ae9d91d1f43ab818f4ad2
Blackfyre33 <mahmutcangonultas@outlook.com>
2026-05-23 17:29:57 +0300

    feat: setup express server with postgres connection

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const pool = require('./db');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Server is runnig' });
+});
+
+app.get('/db-test', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ time: result.rows[0] });
+  } catch (error) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
